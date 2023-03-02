457ca3b7663d8b90214ec2dfd40c3a22335540e7
amansharma-mo <amnsharma@mediaocean.com>
2023-03-02 13:10:06 -0500

    Added weather and input components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Cinput from "./components/cityInput";
+import Whether from "./components/whether";
+import { CityProvider } from "./hooks/useCity";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CityProvider>
+        <Cinput />
+        <Whether />
+      </CityProvider>
     </div>
   );
 }
